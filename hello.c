e24210780a13d599fb4ebe2e2c3382e224aef691
vrml110 <houyue0073@126.com>
2014-12-30 08:50:38 +0800

    update code

---
+++ hello.c
@@ -0,0 +1,30 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  hello.c
+ *
+ *    Description:  hello
+ *
+ *        Version:  1.0
+ *        Created:  2014-12-30 8:12:04
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  vrml1234@gmail.com
+ *        Company:  vrmlstudio
+ *
+ * =====================================================================================
+ */
+
+#include    <stdio.h>
+#include    <string.h>
+
+int main(int argc, char *argv[])
+{
+    printf("hello,world\n");
+    printf("press any when ready...\n");
+    getchar();
+    return 0;
+}
+
+/* end of hello.c */
